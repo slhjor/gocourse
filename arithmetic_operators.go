9dcda0116141155c23b34c19935cdaf5eb0a737a
slhjor <66707821+slhjor@users.noreply.github.com>
2025-08-07 12:35:36 -0700

    Hello to NamingConv

--- arithmetic_operators.go
+++ arithmetic_operators.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	//Variable declaration
@@ -23,5 +26,25 @@ func main() {
 
 	const p float64 = 22 / 7.0
 	fmt.Println(p)
+
+	//Overflow with signed integers
+	var maxInt int64 = 9223372036854775807 // Maximum value for int64 type in Go
+	fmt.Println(maxInt)
+
+	maxInt = maxInt + 1
+	fmt.Println(maxInt)
+
+	//Overflow with unsigned integers
+	var uMaxInt uint64 = 18446744073709551615 // Maximum value for uint64 type in Go
+	fmt.Println(uMaxInt)
+
+	uMaxInt = uMaxInt + 1
+	fmt.Println(uMaxInt)
+
+	// Underflow with floating point numbers
+	var smallFloat float64 = 1.0e-323
+
+	smallFloat = smallFloat / math.MaxFloat64
+	fmt.Println(smallFloat)
 
 }
